8150c415640d9c74f347e3744a75891e06071ae5
SAMIUL BASIR <samiulb25@gmail.com>
2024-07-01 20:34:20 +0600

    URI Online Judge Problem solution, poblem number 1074 and 1075

--- Remaining 2/main.cpp
+++ Remaining 2/main.cpp
@@ -1,9 +1,20 @@
+// URI Online Judge Problem 1075
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int num;
+
+    cin >> num;
+
+    for(int i = 1; i < 10000; i++){
+        if (i % num == 2){
+            cout << i << endl;
+        }
+    }
+
     return 0;
 }
