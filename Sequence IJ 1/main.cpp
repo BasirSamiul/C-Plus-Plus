3f434d3556d705a0fa63be9bdee40e7039690e39
SAMIUL BASIR <samiulb25@gmail.com>
2024-07-02 22:56:42 +0600

    URI Online Judge 1095 solution

---
+++ Sequence IJ 1/main.cpp
@@ -0,0 +1,16 @@
+// URI Online Judge Problem 1095
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    for(int j = 60; j >= 0; j--){
+            cout << "I=" << i << " " << "J=" << j << "\n";
+            i += 3;
+            j -= 4;
+    }
+    return 0;
+}
