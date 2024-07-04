2a974f31c92342d9c931edc3de07befd1b2a5b0f
SAMIUL BASIR <samiulb25@gmail.com>
2024-07-04 21:56:42 +0600

    URI Online Judge 1099 Solution

--- Sum of Consecutive Odd Numbers II/main.cpp
+++ Sum of Consecutive Odd Numbers II/main.cpp
@@ -6,7 +6,41 @@ using namespace std;
 
 int main()
 {
+    int num, x, y;
 
+    cin >> num;
+
+    for (int i =0; i < num; i++){
+
+        int sum  = 0;
+
+        cin >> x >> y;
+
+        if (x > y){
+            for (int j = y; j < x; j++){
+                if(j != y){
+                    if(j % 2 != 0){
+                        sum += j;
+                    }
+                }
+            }
+            cout << sum << endl;
+        }
+        else if(y > x){
+
+            for (int j = x; j < y; j++){
+                if(j != x){
+                    if(j % 2 != 0){
+                        sum += j;
+                    }
+                }
+            }
+            cout << sum << endl;
+        }
+        else{
+            cout << sum << endl;
+        }
+    }
 
     return 0;
 }
