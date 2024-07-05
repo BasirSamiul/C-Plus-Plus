7c987971451663110b00948cdf184ef88436e89a
SAMIUL BASIR <samiulb25@gmail.com>
2024-07-05 20:23:25 +0600

    URI Online Judge 1115 Solution are added

---
+++ Quadrant/main.cpp
@@ -0,0 +1,37 @@
+//URI Online Judge Problem 1115
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int x, y;
+
+    cin >> x >> y;
+
+    while(x || y){
+
+        if(x == 0 || y == 0 ){
+            break;
+        }
+        else if(x > 0 && y > 0){
+            cout << "primeiro" << endl;
+        }
+        else if(x > 0 && y < 0){
+            cout << "quarto" << endl;
+        }
+        else if(x < 0 && y > 0){
+            cout << "segundo" << endl;
+        }
+        else if(x < 0 && y < 0){
+            cout << "terceiro" << endl;
+        }
+
+        cin >> x >> y;
+
+    }
+
+    return 0;
+}
