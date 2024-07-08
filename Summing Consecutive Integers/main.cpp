1d3519c411a21d7411a461273b2ea04948d92735
SAMIUL BASIR <samiulb25@gmail.com>
2024-07-08 20:21:50 +0600

    URI Online Judge 1149 solution

--- Summing Consecutive Integers/main.cpp
+++ Summing Consecutive Integers/main.cpp
@@ -6,6 +6,19 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int algo, num, sum = 0;
+
+    cin >> algo >> num;
+
+    while(num <= 0){
+        cin >> num;
+    }
+
+    for (int i = 1; i <= num; i++){
+            sum += algo;
+            ++algo;
+        }
+    cout << sum << endl;
+
     return 0;
 }
