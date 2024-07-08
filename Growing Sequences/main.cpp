0a547cfa759498953017238ce601e7a575265f3e
SAMIUL BASIR <samiulb25@gmail.com>
2024-07-08 19:43:38 +0600

    URI Online Judge 1146 Problem solution

---
+++ Growing Sequences/main.cpp
@@ -0,0 +1,28 @@
+//URI Online Judge Problem 1146
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cin >> num;
+
+    while(num){
+        if(num == 0){
+            break;
+        }
+        else{
+            for(int i = 1; i <= num; i++){
+                cout << i;
+                cout << ((i == num) ? "\n" : " ");
+            }
+        }
+        cin >> num;
+
+    }
+
+    return 0;
+}
