03ff4266e793175bb5b34a52222322fe2f9894d4
SAMIUL BASIR <samiulb25@gmail.com>
2024-07-09 22:37:08 +0600

    URI Online Judge Problem 1155 solution

---
+++ S Sequence/main.cpp
@@ -0,0 +1,12 @@
+// Uri Online Judge porblem 1155
+
+#include <stdio.h>
+
+int main(){
+    float soma = 0;
+    for(int i = 1; i <= 100; i++){
+            soma += 1/(float)i;
+    }
+    printf("%.2f\n",soma);
+    return 0;
+}
