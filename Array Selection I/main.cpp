8da8e3b1442c64ec2d43308c3e49b37d308fea1d
SAMIUL BASIR <samiulb25@gmail.com>
2024-07-12 13:58:53 +0600

    URI Online Judge 1174 solution

---
+++ Array Selection I/main.cpp
@@ -0,0 +1,36 @@
+//URI Online Judge 1174
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+
+    double arr[100];
+
+    cout << fixed;
+
+    double num;
+
+    for (int i = 0; i < 10; i++){
+
+        cin >> num;
+
+        arr[i] = num;
+
+    }
+
+    for (int i = 0; i < 10; i++){
+
+        if(arr[i] <= 10){
+
+            cout << "A" << "[" << i << "] = " << setprecision(1) << arr[i] << endl;
+
+        }
+
+    }
+
+    return 0;
+}
