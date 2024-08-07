0a3814528f3f9d6fc7a1790f61d37712d8996865
SAMIUL BASIR <samiulb25@gmail.com>
2024-08-07 15:45:41 +0600

    Uri Online Judge 1184 Problem Solution are added

---
+++ Below the Main Diagonal/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+   double M[12][12];
+
+   string letter;
+
+   double num, sum = 0, avg = 0, counter = 0;
+
+   cin >> letter;
+
+   cout << fixed;
+
+   if("S" == letter){
+
+        for(int i = 0; i <= 11; i++){
+
+            for(int j = 0; j <= 11; j++){
+
+                cin >> num;
+
+                M[i][j] = num;
+
+                if(i > j){
+
+                     sum += M[i][j];
+                }
+
+            }
+        }
+
+        cout << setprecision(1) << sum << endl;
+
+   }
+
+   if("M" == letter){
+
+        for(int i = 0; i <= 11; i++){
+
+            for(int j = 0; j <= 11; j++){
+
+                cin >> num;
+
+                M[i][j] = num;
+
+                if(i > j){
+                     counter++;
+                     sum += M[i][j];
+                }
+            }
+        }
+
+        avg = sum / counter;
+
+        cout << setprecision(1) << avg << endl;
+
+   }
+
+}
