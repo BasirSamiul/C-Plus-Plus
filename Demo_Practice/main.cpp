43a8b4af59622745bbf8dba9914849ab38cba5b6
SAMIUL BASIR <samiulb25@gmail.com>
2024-08-10 23:55:13 +0600

    For Deomo practice

---
+++ Demo_Practice/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string my_string = "My first string";
+    string my_string1 ("My second string");
+    string my_string2{"This is my last string"};
+
+    cout <<
+    my_string << endl << my_string1 << endl << my_string2 << endl;
+
+    return 0;
+}
